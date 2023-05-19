9d67d40b3c7cbf83169ba02f2659c6f9d6d1d6ec
Ramasamy Kandasamy <ramasamy.kandasamy@BLRETV-C02FF3D2.local>
2023-05-19 06:48:10 +0530

    Implement skeleton code for client

--- src/com/flipkart/client/CRSApplicationClient.java
+++ src/com/flipkart/client/CRSApplicationClient.java
@@ -2,10 +2,10 @@
  * 
  */
 package com.flipkart.client;
-import java.util.*;
-import com.flipkart.service.*;
+import java.util.Scanner;
+
 /**
- * @author ansh.agarwal
+ * @author ramasamy.kandasamy
  *
  */
 public class CRSApplicationClient {
@@ -13,44 +13,137 @@ public class CRSApplicationClient {
 	/**
 	 * @param args
 	 */
-	
-	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
+		
+		Scanner scannerObj = new Scanner(System.in);
 		int choice;
+		
 		System.out.println("Welcome to CRS Application");
-		System.out.println("MENU FOR OPERARION");
-		System.out.println("1. Login");
-		System.out.println("2. Registration of the Student");
-		System.out.println("3. Update Password");
-		System.out.println("4. Exit");
-		Scanner scn=new Scanner(System.in);
-		choice=scn.nextInt();
-		switch(choice)
-		{
-			case 1 :  
-				LoginOperationService login=new LoginOperationService();
-				String username, password, role;
-				System.out.println("Enter name: ");
-			    username=scn.nextLine();
-					  System.out.println("Enter password: ");
-					  password=scn.nextLine();
-					  System.out.println("Enter role: ");
-					  role=scn.nextLine();
-					  login.show();
-					  if(role.equals("Student"))
-					  {
-						 CRSStudentMenu.displayMenu();
-						  
-					  }
-					  break;
-			case 2 :  
-			case 3 :
-			case 4 :
-			default: System.out.println("PLease enter a valid choice.");
+		
+		
+		
+		boolean interactWithClient = true;
+		while(interactWithClient){
+			// Take input choice until the user chooses to exit.
+			System.out.println("Select one of the following choices by inputting the choice number:");
+			System.out.println("1. Login");
+			System.out.println("2. New student registration");
+			System.out.println("3. Update Password");
+			System.out.println("4. Exit");
+
+			// Get user choice and act on it.
+			choice = scannerObj.nextInt();
+			scannerObj.nextLine();
+			switch(choice) {
+			case 1:
+				System.out.println("You have chosen: 1. Login");
+				login();
+				break;
+			case 2: 
+				// Do new student registration.
+				break;
+			case 3:
+				// Procedures for update password.
+				break;
+			case 4:
+				interactWithClient = false; // Exit the service.
+				break;
+			default:
+				System.out.println("Error!! Input valid choices.");
+				// Write code to handle this.
+			
+			}
 		}
-		scn.close();
+		//scannerObj.close();
 		
 	}
+	
+	private static boolean login() {
+		Scanner scannerObj = new Scanner(System.in);
+		
+		// Get ID.
+		System.out.println("Enter your ID");
+		int id = scannerObj.nextInt();
+		scannerObj.nextLine(); // Consumes the empty return left by nextInt in the previous line.
+		
+		// Get Password.
+		System.out.println("Enter your password");
+		String password = scannerObj.nextLine();
+		
+		// Get user role.
+		System.out.println("Enter you position");
+		String role = scannerObj.nextLine();
+		
+		// Check if the user exists
+		if(!userExists(id)) {
+			System.out.println("You are not a verified user.");
+			if(role.equals("Student")) {
+				System.out.println("Register yourself as a new student");
+			} else if(role.equals("Professor")) {
+				System.out.println("Contact the Admin.");
+			} else {
+				// This condition should not happen.
+				// Handle the exception. <-- TODO
+			}
+			return false;
+		}
+		
+		
+		// Check if password and role matches.
+		if(!verifyPassword(id, password)) {
+			return false;
+		}
+		
+		if(!verifyRole(id, role)) {
+			return false;
+		}
+		
+		
+		// Call user specific operations.
+		
+		if (role.equals("admin")) {
+			CRSAdminMenu.process();
+		} else if(role.equals("professor")) {
+			CRSProfessorMenu.process();
+		} else if(role.equals("student")) {
+			CRSStudentMenu.process();
+		} else {
+			// Error. Handle the error.
+			// May be this is not necessary as we check above if the role matches.ß
+		}
+		
+		return true;
+	}
 
+	private static boolean userExists(int id) {
+		// TODO Implement this function.
+		return true;
+	}
+	
+	private static boolean verifyPassword(int id, String inputPassword) {
+		return true;
+	}
+	
+	private static boolean verifyRole(int id, String inputRole) {
+		return true;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/com/flipkart/client/CRSProfessorMenu.java
+++ src/com/flipkart/client/CRSProfessorMenu.java
@@ -3,10 +3,48 @@
  */
 package com.flipkart.client;
 
+import java.util.Scanner;
+
 /**
- * @author ansh.agarwal
+ * @author ramasamy.kandasamy
  *
  */
 public class CRSProfessorMenu {
+	public static void displayMenu() {
+		System.out.println("1. Generate Report Card");
+		System.out.println("2. Add Professor");
+		System.out.println("3. Exit");
+	}
+	
+	public static int getChoice() {
+		displayMenu();
+		Scanner scannerObj = new Scanner(System.in);
+		int choice = scannerObj.nextInt();
+		scannerObj.nextLine();
+		return choice;
+	}
 
+	
+	public static void process() {
+		int choice;
+		boolean terminate = false;
+		while(!terminate) {
+			choice = getChoice();
+			switch(choice) {
+			case 1:
+				// pass
+				break;
+			case 2:
+				//pass
+				break;
+			case 3:
+				//pass
+				terminate = true;
+				break;
+			default:
+				System.out.println("Error! Invalid choice.");
+				// Handle the exception.
+			}
+		}
+	}
 }
